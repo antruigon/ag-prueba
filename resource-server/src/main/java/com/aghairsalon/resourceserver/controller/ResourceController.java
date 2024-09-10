1a2afc5e52b55c8c7c24c9125f391ca316d43ef3
Antonio Ruiz <antoniorg2910@gmail.com>
2024-09-10 12:02:28 +0200

    Resource server añadido

---
+++ resource-server/src/main/java/com/aghairsalon/resourceserver/controller/ResourceController.java
@@ -0,0 +1,26 @@
+package com.aghairsalon.resourceserver.controller;
+
+import com.aghairsalon.resourceserver.dto.MessageDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/resource")
+public class ResourceController {
+
+    @GetMapping("/user")
+    public ResponseEntity<MessageDTO> user(Authentication authentication) {
+        return ResponseEntity.ok(new MessageDTO("Hola " + authentication.getName()));
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<MessageDTO> admin(Authentication authentication) {
+        return ResponseEntity.ok(new MessageDTO("Hola Mr." + authentication.getName()));
+    }
+
+}
