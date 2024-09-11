758071cc5e33295671ff6bcc2451744931f77a81
Antonio Ruiz <antoniorg2910@gmail.com>
2024-09-11 09:37:35 +0200

    Angular con login

---
+++ ag-prueba-front/src/app/components/menu/menu.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Router } from '@angular/router';
+import { HttpParams } from '@angular/common/http';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent implements OnInit {
+  
+  authorize_uri = environment.authorized_uri;
+
+  params: any =  {
+    client_id: environment.client_id,
+    redirect_uri: environment.redirect_uri,
+    scope: environment.scope,
+    response_type: environment.response_type,
+    response_mode: environment.response_mode,
+    code_challenge_method: environment.code_challenge_method,
+    code_challenge: environment.code_challenge,
+  }
+
+  constructor(private router: Router) {  }
+
+  ngOnInit(): void {
+  }
+
+  onLogin(): void {
+    const httpParams = new HttpParams({fromObject: this.params});
+    const codeUrl = this.authorize_uri + httpParams.toString();
+    location.href = codeUrl;
+  }
+
+  onLogout(): void {
+
+  }
+
+}
---
+++ ag-prueba-front/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    authorized_uri: 'http://localhost:9000/oauth2/authorize?',
+    client_id: 'client',
+    redirect_uri: 'http://127.0.0.1:4200/authorized',
+    scope: 'openid profile',
+    response_type: 'code',
+    response_mode: 'form_post',
+    code_challenge_method: 'S256',
+    code_challenge: 'nYj265odKbrvyld8kxovZqAE6hnCuSUuhMnwXXSjq3w',
+    code_verifier: 'CE5SxaQv8clIx5DDb9xrkTpKlrCInLAvaDJInUoQtw6',
+    token_url: 'http://localhost:9000/oauth2/token'
+};
